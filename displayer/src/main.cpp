6df0ef1e4411f156b24a24286b3e05157a3a39c0
Julien Carayon <78759372+JulienCarayon@users.noreply.github.com>
2023-04-14 17:09:53 +0200

    :sparkles: added error management

--- displayer/src/main.cpp
+++ displayer/src/main.cpp
@@ -7,30 +7,40 @@ public:
         std::cout << "Message received on topic " << msg->get_topic() << std::endl;
         std::cout << "Message content: " << msg->to_string() << std::endl;
     }
+
+    virtual void connection_lost(const std::string& cause) override {
+        std::cout << "\nConnection lost" << std::endl;
+        if (!cause.empty())
+            std::cout << "\tcause: " << cause << std::endl;
+    }
 };
 
 int main() {
     std::string address = "tcp://broker.emqx.io:1883";
     std::string client_id = "MQTT-Receiver";
     std::string topic = "/ynov/bordeaux/";
-
     mqtt::async_client client(address, client_id);
+    try {
+        mqtt::connect_options conn_opts;
+        conn_opts.set_clean_session(true);
 
-    mqtt::connect_options conn_opts;
-    conn_opts.set_clean_session(true);
+        callback cb;
+        client.set_callback(cb);
 
-    callback cb;
-    client.set_callback(cb);
+        client.connect(conn_opts)->wait();
 
-    client.connect(conn_opts)->wait();
+        mqtt::topic mqtt_topic(client, topic, 0);
+        mqtt_topic.subscribe()->wait();
 
-    mqtt::topic mqtt_topic(client, topic, 0);
-    mqtt_topic.subscribe()->wait();
-
-    while (true) {
-        // Wait for messages
+        while (true) {
+            // Wait for messages
+        }
     }
-
-    client.disconnect()->wait();
+    catch (const mqtt::exception& exc) {
+        client.disconnect()->wait();
+        std::cerr << "MQTT error: " << exc.what() << std::endl;
+        return 1;
+    }
+    
     return 0;
 }
