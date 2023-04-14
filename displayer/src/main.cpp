4921dd6c78075e60ff93a81aa39556c7d045c557
Julien Carayon <78759372+JulienCarayon@users.noreply.github.com>
2023-04-14 19:04:03 +0200

    :art: Followed PCQF given by our professor

--- displayer/src/main.cpp
+++ displayer/src/main.cpp
@@ -1,44 +1,44 @@
 #include <iostream>
 #include <mqtt/async_client.h>
 
-class callback : public virtual mqtt::callback {
+class Callback : public virtual mqtt::callback {
 public:
     void message_arrived(mqtt::const_message_ptr msg) override {
         std::cout << "Message received on topic " << msg->get_topic() << std::endl;
         std::cout << "Message content: " << msg->to_string() << std::endl;
     }
 
-    virtual void connection_lost(const std::string& cause) override {
+    virtual void connection_lost(const std::string& cause_ref) override {
         std::cout << "\nConnection lost" << std::endl;
-        if (!cause.empty())
-            std::cout << "\tcause: " << cause << std::endl;
+        if (!cause_ref.empty())
+            std::cout << "\tcause: " << cause_ref << std::endl;
     }
 };
 
 int main() {
-    std::string address = "tcp://broker.emqx.io:1883";
-    std::string client_id = "MQTT-Receiver";
-    std::string topic = "/ynov/bordeaux/";
-    mqtt::async_client client(address, client_id);
+    const std::string s_ADDRESS = "tcp://broker.emqx.io:1883";
+    const std::string s_CLIENT_ID = "MQTT-Receiver";
+    const std::string s_TOPIC = "/ynov/bordeaux/";
+    mqtt::async_client client_t(s_ADDRESS, s_CLIENT_ID);
     try {
         mqtt::connect_options conn_opts;
         conn_opts.set_clean_session(true);
 
-        callback cb;
-        client.set_callback(cb);
+        Callback callback_t;
+        client_t.set_callback(callback_t);
 
-        client.connect(conn_opts)->wait();
+        client_t.connect(conn_opts)->wait();
 
-        mqtt::topic mqtt_topic(client, topic, 0);
+        mqtt::topic mqtt_topic(client_t, s_TOPIC, 0);
         mqtt_topic.subscribe()->wait();
 
         while (true) {
             // Wait for messages
         }
     }
-    catch (const mqtt::exception& exc) {
-        client.disconnect()->wait();
-        std::cerr << "MQTT error: " << exc.what() << std::endl;
+    catch (const mqtt::exception& exc_ref) {
+        client_t.disconnect()->wait();
+        std::cerr << "MQTT error: " << exc_ref.what() << std::endl;
         return 1;
     }
     
--- message_sender/src/main.cpp
+++ message_sender/src/main.cpp
@@ -1,41 +1,41 @@
 #include <iostream>
 #include <mqtt/async_client.h>
 
-const std::string ADDRESS { "tcp://broker.emqx.io:1883" };
-const std::string CLIENT_ID { "MQTT-Receiver" };
-const std::string TOPIC { "/ynov/bordeaux/" };
+const std::string s_ADDRESS { "tcp://broker.emqx.io:1883" };
+const std::string s_CLIENT_ID { "MQTT-Receiver" };
+const std::string s_TOPIC { "/ynov/bordeaux/" };
 
-class callback : public virtual mqtt::callback
+class Callback : public virtual mqtt::callback
 {
 public:
-    virtual void connection_lost(const std::string& cause) {
+    virtual void connection_lost(const std::string& cause_ref) {
         std::cout << "Connection lost" << std::endl;
     }
 
     virtual void delivery_complete(mqtt::delivery_token_ptr token) {}
 
-    virtual void message_arrived(const std::string& topic, mqtt::message_ptr msg) {}
+    virtual void message_arrived(const std::string& topic_ref, mqtt::message_ptr msg) {}
 };
 
 int main()
 {
     try {
-        mqtt::async_client client(ADDRESS, CLIENT_ID);
-        callback cb;
-        client.set_callback(cb);
+        mqtt::async_client client(s_ADDRESS, s_CLIENT_ID);
+        Callback Callback;
+        client.set_callback(Callback);
 
         mqtt::connect_options connOpts;
         connOpts.set_keep_alive_interval(20);
         connOpts.set_clean_session(true);
 
-        std::cout << "Connecting to broker: '" << ADDRESS << "'..." << std::endl;
+        std::cout << "Connecting to broker: '" << s_ADDRESS << "'..." << std::endl;
         mqtt::token_ptr conntok = client.connect(connOpts);
         conntok->wait();
 
         std::cout << "Connected" << std::endl;
 
         std::cout << "Sending message..." << std::endl;
-        mqtt::message_ptr pubmsg = mqtt::make_message(TOPIC, "coucou c'est l'homme le plus intelligent qui envoie un message\nPS : Merci ChatGPT");
+        mqtt::message_ptr pubmsg = mqtt::make_message(s_TOPIC, "coucou c'est l'homme le plus intelligent qui envoie un message\nPS : Merci ChatGPT");
         pubmsg->set_qos(0);
         client.publish(pubmsg)->wait();
 
